4cc758801fffb39d6f2023a928e9b2beb88b9ac0
alper <alperakbulut@gmail.com>
2023-09-03 14:02:58 +0200

    fix(import controller): fix error on the first opening of the page

--- src/Controller/ExcelImportController.php
+++ src/Controller/ExcelImportController.php
@@ -13,12 +13,17 @@ class ExcelImportController extends AbstractController
     #[Route('/excel/import', name: 'app_excel_import')]
     public function index(Request $request): Response
     {
-        /** @var UploadedFile $uploadedFile */
-        $uploadedFile = $request->files->get('excel_file');
-        $destination = $this->getParameter('kernel.project_dir') . '/public/uploads';
-        $newFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $extension = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_EXTENSION);
-        $uploadedFile->move($destination, $newFilename . '-' . uniqid() . '.' . $extension);
+        if ($request->isMethod('POST')) {
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $request->files->get('excel_file');
+            $destination = $this->getParameter('kernel.project_dir') . '/public/uploads';
+            $newFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $extension = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_EXTENSION);
+            $uploadedFile->move($destination, $newFilename . '-' . uniqid() . '.' . $extension);
+            $this->addFlash('success', 'L\'importation du fichier Excel a été effectuée avec succès.');
+
+            return $this->redirectToRoute('app_excel_import');
+        }
 
         return $this->render('excel_import/index.html.twig', [
             'controller_name' => 'ExcelImportController',
